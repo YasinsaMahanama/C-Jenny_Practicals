0497b8214b27ab746d8bb764091fa164570cd70b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-29 17:11:30 +0530

    Update a3.c

--- Functions in C/a3.c
+++ Functions in C/a3.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 
-void sum(int x,int y); //function declaration
+void sum(int,int); //function declaration
 void main()
 {
     int x=3, y=4;
@@ -11,12 +11,6 @@ void main()
 void sum(int a,int b)      //function definition
 {
     int sum=0;
-    printf("Enter the value of numbers : \n");
-    printf("\ta = ");
-    scanf("%d",&a);
-    printf("\tb = ");
-    scanf("%d",&b);
-
     sum = a+b;
     printf("\nSUM = %d",sum);
 

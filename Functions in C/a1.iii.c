ef0e04a65caf8e05e575d1b8759a0a2838cc78bd
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-29 16:51:54 +0530

    Create a1.iii.c

---
+++ Functions in C/a1.iii.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void sum(int a,int b)      //function definition
+{
+    int sum=0;
+    printf("Enter the value of numbers : \n");
+    printf("\ta = ");
+    scanf("%d",&a);
+    printf("\tb = ");
+    scanf("%d",&b);
+
+    sum = a+b;
+    printf("\nSUM = %d",sum);
+
+getch();
+}
+
+void main()
+{
+    sum(5,7);      //function calling
+}
+

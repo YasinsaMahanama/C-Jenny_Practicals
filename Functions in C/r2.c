2ebcb2e454cbad6a4d275ede01a726b7ed015c1b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-30 17:20:22 +0530

    Add files via upload

---
+++ Functions in C/r2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int sum(int x)
+{
+    int s = 0;
+    if(x==1)
+    {
+        return x;
+    }
+    s = x+sum(x-1);
+    return s;
+}
+
+void main()
+{
+    int a;
+    a = sum(5);
+    printf("%d\n",a);
+getch();
+}
